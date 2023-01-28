71191b51e810e8521c606206d756a401146ef60e
KyusungSeo <seo3650@kaist.ac.kr>
2023-01-28 13:43:48 +0900

    Implement kakao pay

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -17,8 +17,9 @@ export const serverSchema = z.object({
     // Since NextAuth.js automatically uses the VERCEL_URL if present.
     (str) => process.env.VERCEL_URL ?? str,
     // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-    process.env.VERCEL ? z.string() : z.string().url(),
+    process.env.VERCEL ? z.string() : z.string().url()
   ),
+  APP_ADMIN_KEY: z.string(),
   KAKAO_CLIENT_ID: z.string(),
   KAKAO_CLIENT_SECRET: z.string(),
 });
@@ -33,6 +34,7 @@ export const serverEnv = {
   NODE_ENV: process.env.NODE_ENV,
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  APP_ADMIN_KEY: process.env.APP_ADMIN_KEY,
   KAKAO_CLIENT_ID: process.env.KAKAO_CLIENT_ID,
   KAKAO_CLIENT_SECRET: process.env.KAKAO_CLIENT_SECRET,
 };
@@ -43,7 +45,7 @@ export const serverEnv = {
  * To expose them to the client, prefix them with `NEXT_PUBLIC_`.
  */
 export const clientSchema = z.object({
-  // NEXT_PUBLIC_CLIENTVAR: z.string(),
+  NEXT_PUBLIC_BASE_URL: z.string(),
 });
 
 /**
@@ -53,5 +55,5 @@ export const clientSchema = z.object({
  * @type {{ [k in keyof z.infer<typeof clientSchema>]: z.infer<typeof clientSchema>[k] | undefined }}
  */
 export const clientEnv = {
-  // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+  NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
 };
