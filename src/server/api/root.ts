57ceafdec2943c6fcda31f29a61f814a121fc10c
JungInRhee <ysrheee@kaist.ac.kr>
2023-01-28 12:15:28 +0900

    remove example router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from "./trpc";
-import { exampleRouter } from "./routers/example";
 import { eventRouter } from "./routers/event";
 
 /**
@@ -8,7 +7,6 @@ import { eventRouter } from "./routers/event";
  * All routers added in /api/routers should be manually added here
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   event: eventRouter
 });
 
