6243929b04acc812bfa2a719ff5c01cb0ce4eebf
KyusungSeo <seo3650@kaist.ac.kr>
2023-01-28 13:43:48 +0900

    Implement kakao pay

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -2,6 +2,7 @@ import { createTRPCRouter } from "./trpc";
 import { exampleRouter } from "./routers/example";
 import { payRouter } from "./routers/pay";
 import { eventRouter } from "./routers/event";
+import { payRouter } from "./routers/pay";
 
 /**
  * This is the primary router for your server.
@@ -12,6 +13,7 @@ export const appRouter = createTRPCRouter({
   example: exampleRouter,
   pay: payRouter,
   event: eventRouter,
+  pay: payRouter,
 });
 
 // export type definition of API
