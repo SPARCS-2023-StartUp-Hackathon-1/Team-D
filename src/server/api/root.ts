fb94974eb4d8317feac01d3e74a0bf74925a7119
KyusungSeo <seo3650@kaist.ac.kr>
2023-01-28 14:41:46 +0900

    Remove redundant code

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from "./trpc";
-import { exampleRouter } from "./routers/example";
 import { payRouter } from "./routers/pay";
 import { eventRouter } from "./routers/event";
 import { payRouter } from "./routers/pay";
@@ -10,10 +9,8 @@ import { payRouter } from "./routers/pay";
  * All routers added in /api/routers should be manually added here
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   pay: payRouter,
   event: eventRouter,
-  pay: payRouter,
 });
 
 // export type definition of API
