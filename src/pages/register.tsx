a59d95c27797370f29aae4043081f6c3b298acad
dongjoo0-0 <kdj486347@gmail.com>
2023-01-28 20:08:41 +0900

    complete register

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,18 +1,27 @@
 import { useSession } from "next-auth/react";
-import { FormEvent, useEffect } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { api } from "../utils/api";
 import { ProfileImage } from "../components/profileImage";
 import { useRouter } from "next/router";
 
 const Register = () => {
   const { data: session } = useSession();
+  const [name, setName] = useState("");
   const router = useRouter();
   const { mutate } = api.user.updateName.useMutation();
+
+  useEffect(() => {
+    if (session) {
+      setName(session.user?.name ?? "");
+    }
+  }, [session])
   
   const handleRegister = (event : FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    
+    mutate({ name })
+
+    void router.push('/');
   }
 
   if (session) {
@@ -30,15 +39,16 @@ const Register = () => {
           <input 
             type="text" 
             name="name" 
-            value={session.user?.name ? session.user?.name : ""}
+            value={name}
             style={{backgroundColor: "rgba(255, 255, 255, 0.35)", fontFamily: "NanumSquareRoundEB", fontSize: "14px"}}
             className="rounded-xl w-80 h-11 p-4 text-primary"
+            onChange={(event)=> {setName(event.target.value)}}
           />
           <button 
             type="submit"
-            className="w-80 h-11 text-center bg-primary"
+            className="w-80 h-11 text-center bg-primary text-white rounded-xl fixed bottom-20 left-1/2 -ml-40"
           >
-              다음
+            다음
           </button>
         </form>
       </div>
