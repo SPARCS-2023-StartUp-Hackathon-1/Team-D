bbe7fc883de09105650eb923314e7286f87e899a
JungInRhee <ysrheee@kaist.ac.kr>
2023-01-28 16:51:08 +0900

    fix profile image

--- src/components/profileImage.tsx
+++ src/components/profileImage.tsx
@@ -5,5 +5,5 @@ interface props {
 }
 
 export const ProfileImage = ({src}: props) => {
-  return <Image className="m-2 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500" src={src} alt = "profile image" width={80} height={80} />
+  return <Image className="p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500" src={src} alt = "profile image" width={80} height={80} />
 }
