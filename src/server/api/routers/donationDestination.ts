1421ead1eea699c698c531221b77ccbb78e017be
KyusungSeo <seo3650@kaist.ac.kr>
2023-01-29 11:49:40 +0900

    Remove redundant

--- src/server/api/routers/donationDestination.ts
+++ src/server/api/routers/donationDestination.ts
@@ -1,4 +1,3 @@
-import { Color } from '@prisma/client';
 import { z } from 'zod';
 
 import { createTRPCRouter, protectedProcedure, publicProcedure } from '../trpc';
@@ -13,42 +12,5 @@ export const donationDestinationRouter = createTRPCRouter({
       });
       console.log(res);
       return res;
-    }),
-
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-  createDonation: protectedProcedure
-    .input(
-      z.object({
-        name: z.string(),
-        amount: z.number(),
-        message: z.string(),
-        eventId: z.string()
-      })
-    )
-    .mutation(async ({ ctx, input }) => {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-      await ctx.prisma.donate.create({
-        data: {
-          donorName: input.name,
-          money: input.amount,
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-          color: Color.RED,
-          message: input.message,
-          eventId: input.eventId
-        }
-      });
-
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
-      return await ctx.prisma.event.update({
-        where: { id: input.eventId },
-        data: {
-          totalDonation: {
-            increment: input.amount
-          },
-          numDonors: {
-            increment: 1
-          }
-        }
-      });
     })
 });
